de89c2f99f97174e3a6b35e60f8ee5c933891522
Polina <sirotkina.polina.2001@gmail.com>
2021-03-16 17:09:17 +0300

    update file's pushbutton

--- lab_01/mainwindow.cpp
+++ lab_01/mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_mainwindow.h"
 #include <iostream>
 #include <string>
+#include <QTextStream>
+#include <fstream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -21,8 +24,25 @@ void MainWindow::on_pushButton_clicked()
     this->close();
 }
 
-void MainWindow::on_pushButton_5_clicked()
+void MainWindow::on_pushButton_5_pressed()
 {
+    QTextStream out(stdout);
     QString val = ui->lineEdit->text();
-    ui->lineEdit->setText("5");
+    std::string a = (val).toStdString(), b = (val).toStdString();
+    std::ifstream f(a.c_str(), std::ios_base::in);
+    if (f.is_open())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Ошибка");
+        msg.setText("Невозможно открыть файл с заданным названием.");
+        msg.setIcon(QMessageBox::Critical);
+        msg.exec();
+    }
+    else
+    {
+        int a;
+        f >> a;
+        std::cout << "First num in file: " << a;
+        f.close();
+    }
 }
