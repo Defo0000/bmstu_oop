d1a0d44f4132c5374f382ae35ebca846721ade20
Polina <sirotkina.polina.2001@gmail.com>
2021-03-23 14:31:14 +0300

    add init drawing

--- lab_01/mainwindow.cpp
+++ lab_01/mainwindow.cpp
@@ -1,36 +1,166 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QPainter>
+#include <QVector3D>
+#include <chrono>
+#include <ctime>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include <iostream>
 #include <string>
 #include <QTextStream>
 #include <fstream>
 #include <QMessageBox>
 
+QGraphicsScene *scene;
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+
+  scene = new QGraphicsScene();
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+
+  delete scene;
+
+  delete ui;
 }
 
+void MainWindow::on_pushButton_clicked() {
 
-void MainWindow::on_pushButton_clicked()
+    scene->clear();
+    ui->graphicsView->items().clear();
+    QImage image = QImage(1061, 941, QImage::Format_RGB32);
+    QPainter p(&image);
+    p.setBrush(QColor(0, 0, 0));
+    p.setPen(QPen(QColor(0, 0, 0), 3));
+
+    QPolygon back = QPolygon();
+    QPoint buf(0, 0);
+    back << buf;
+    buf = QPoint(1061, 941);
+    back << buf;
+    buf = QPoint(1061, 0);
+    back << buf;
+    p.drawPolygon(back);
+
+    p.setPen(QPen(QColor(173, 237, 190), 3));
+
+    QPolygon polygon = QPolygon();
+    buf = QPoint(1, 1);
+    polygon << buf;
+    buf = QPoint(20, 20);
+    polygon << buf;
+    buf = QPoint(20, 1);
+    polygon << buf;
+    p.drawPolygon(polygon);
+
+    for (int i = 0; i < 300; i++) {
+      QPoint a(i, i);
+      p.drawPoint(a);
+    }
+
+    QPixmap pixmap = QPixmap::fromImage(image);
+    scene->addPixmap(pixmap);
+    ui->graphicsView->setScene(scene);
+}
+
+void MainWindow::on_MainWindow_destroyed() {}
+
+void MainWindow::add_point(int x, int y)
 {
-    this->close();
+    QImage image = QImage(1000, 900, QImage::Format_RGB32);
+    QPainter p(&image);
+    p.setBrush(QColor(0, 100, 0));
+    p.setPen(QPen(QColor(255, 0, 0), 10));
+    QPoint a(x, y);
+    p.drawLine(1000, 900, x, y);
+    ui->graphicsView->scene()->update();
+
+    QPixmap pixmap = QPixmap::fromImage(image);
+    scene->addPixmap(pixmap);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->scene()->addPixmap(pixmap);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    scene->clear();
+    ui->graphicsView->items().clear();
+    QImage image = QImage(1000, 900, QImage::Format_RGB32);
+    QPainter p(&image);
+
+    p.setBrush(QColor(0, 100, 0));
+    p.setPen(QPen(QColor(173, 237, 190), 3));
+
+    add_point(300, 100);
+
+    QPixmap pixmap = QPixmap::fromImage(image);
+    scene->addPixmap(pixmap);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->scene()->addPixmap(pixmap);
+}
+
+struct point
+{
+    double x, y, z;
+};
+using point_t = struct point;
+
+void MainWindow::load_data(std::ifstream& f)
+{
+
+    QImage image = QImage(1000, 900, QImage::Format_RGB32);
+    QPainter p(&image);
+    p.setBrush(QColor(0, 100, 0));
+    p.setPen(QPen(QColor(173, 237, 190), 5));
+
+    int n;
+    f >> n;
+    point_t data[n];
+    for (int i = 0; i < n; i++)
+        f >> data[i].x >> data[i].y >> data[i].z;
+    int x_screen, y_screen, k = 1000;
+    for (int i = 0; i < n; i++)
+    {
+        x_screen = k * data[i].x / (data[i].z + k);
+        y_screen = k * data[i].y / (data[i].z + k);
+        p.drawPoint(x_screen, y_screen);
+        std::cout << data[i].x << " " << data[i].y << " " << data[i].z << "\n";
+    }
+
+    int m;
+    f >> m;
+    int src, dst, x1_screen, y1_screen, x2_screen, y2_screen;
+    for (int i = 0; i < m; i++)
+    {
+        f >> src >> dst;
+        src--;
+        dst--;
+        x1_screen = k * data[src].x / (data[src].z + k);
+        y1_screen = k * data[src].y / (data[src].z + k);
+        x2_screen = k * data[dst].x / (data[dst].z + k);
+        y2_screen = k * data[dst].y / (data[dst].z + k);
+
+        p.drawLine(x1_screen, y1_screen, x2_screen, y2_screen);
+    }
+
+    QPixmap pixmap = QPixmap::fromImage(image);
+    scene->addPixmap(pixmap);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->scene()->addPixmap(pixmap);
 }
 
 void MainWindow::on_pushButton_5_pressed()
 {
-    QTextStream out(stdout);
-    QString val = ui->lineEdit->text();
-    std::string a = (val).toStdString(), b = (val).toStdString();
-    std::ifstream f(a.c_str(), std::ios_base::in);
-    if (f.is_open())
+    std::ifstream f(ui->lineEdit->text().toStdString().c_str(), std::ios_base::in);
+    if (!f)
     {
         QMessageBox msg;
         msg.setWindowTitle("Ошибка");
@@ -40,9 +170,12 @@ void MainWindow::on_pushButton_5_pressed()
     }
     else
     {
-        int a;
-        f >> a;
-        std::cout << "First num in file: " << a;
+        load_data(f);
         f.close();
     }
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    this->close();
 }
--- lab_01/mainwindow.h
+++ lab_01/mainwindow.h
@@ -2,26 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+  void add_point(int x, int y);
+  void load_data(std::ifstream& f);
 
 private slots:
-    void on_pushButton_clicked();
 
-    void on_pushButton_5_pressed();
+  void on_pushButton_clicked();
+
+  void on_MainWindow_destroyed();
+
+  void on_pushButton_2_clicked();
+
+  void on_pushButton_8_clicked();
+
+  void on_pushButton_5_pressed();
+
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
